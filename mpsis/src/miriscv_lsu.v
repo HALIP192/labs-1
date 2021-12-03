f46a2dbcb808683c041e656745b89a1c536c1de4
Yosiky <48320603+Yosiky@users.noreply.github.com>
2021-12-03 23:35:32 +0300

    Change value lsu_size_i

--- mpsis/src/miriscv_lsu.v
+++ mpsis/src/miriscv_lsu.v
@@ -46,65 +46,65 @@ module miriscv_lsu
 
 	always @(*) begin
 		case(lsu_size_i)
-        'd0: begin 
-                case (offset)
-                    'b00: begin 
-                               data <= {{24{data_rdata_i[7]}}, data_rdata_i[7:0]};
-                               data_be <= 'b0001;
-                          end
-                    'b01: begin
-                                data <= {{24{data_rdata_i[15]}}, data_rdata_i[15:8]};
-                                data_be <= 'b0010;
-                          end
-                    'b10: begin
-                                data <= {{24{data_rdata_i[23]}}, data_rdata_i[23:16]};
-                                data_be <= 'b0100;
-                          end
-                    'b11: begin
-                                data <= {{24{data_rdata_i[31]}}, data_rdata_i[31:24]};     
-                                data_be <= 'b1000;
-                          end
-                endcase
-                data_wdata <= {4{lsu_data_i[7:0]}};
-             end
-        'd1: begin 
-                case (offset)
-                    'b00: begin
-                                data <= {{16{data_rdata_i[15]}}, data_rdata_i[15:0]};
-                                data_be <= 'b0011;
-                          end
-                    'b10: begin
-                                data <= {{16{data_rdata_i[31]}}, data_rdata_i[31:16]};
-                                data_be <= 'b1100;
-                          end
-                endcase
-                data_wdata <= {2{lsu_data_i[15:0]}};
-             end 
-        'd2: begin
-                data <= data_rdata_i;
-                data_be <= 'b1111;
-                data_wdata <= lsu_data_i;
-             end
-        'd3: begin 
-                case (offset)
-                    'b00: data <= {24'b0, data_rdata_i[7:0]};
-                    'b01: data <= {24'b0, data_rdata_i[15:8]};
-                    'b10: data <= {24'b0, data_rdata_i[23:16]};
-                    'b11: data <= {24'b0, data_rdata_i[31:24]};     
-                endcase
-             end 
-        'd4: begin 
-                case (offset)
-                    'b00: data <= {16'b0, data_rdata_i[15:0]};
-                    'b10: data <= {16'b0, data_rdata_i[31:16]};
-                endcase
-             end  
-         default: begin
-                    data_be <= 'b0001;
-                    data <= 'd0;
-                    data_wdata <= 'd0;
-             end
-    endcase
+		'd0: begin 
+				case (offset)
+					'b00: begin 
+							   data <= {{24{data_rdata_i[7]}}, data_rdata_i[7:0]};
+							   data_be <= 'b0001;
+						  end
+					'b01: begin
+								data <= {{24{data_rdata_i[15]}}, data_rdata_i[15:8]};
+								data_be <= 'b0010;
+						  end
+					'b10: begin
+								data <= {{24{data_rdata_i[23]}}, data_rdata_i[23:16]};
+								data_be <= 'b0100;
+						  end
+					'b11: begin
+								data <= {{24{data_rdata_i[31]}}, data_rdata_i[31:24]};     
+								data_be <= 'b1000;
+						  end
+				endcase
+				data_wdata <= {4{lsu_data_i[7:0]}};
+			 end
+		'd1: begin 
+				case (offset)
+					'b00: begin
+								data <= {{16{data_rdata_i[15]}}, data_rdata_i[15:0]};
+								data_be <= 'b0011;
+						  end
+					'b10: begin
+								data <= {{16{data_rdata_i[31]}}, data_rdata_i[31:16]};
+								data_be <= 'b1100;
+						  end
+				endcase
+				data_wdata <= {2{lsu_data_i[15:0]}};
+			 end 
+		'd2: begin
+				data <= data_rdata_i;
+				data_be <= 'b1111;
+				data_wdata <= lsu_data_i;
+			 end
+		'd4: begin 
+				case (offset)
+					'b00: data <= {24'b0, data_rdata_i[7:0]};
+					'b01: data <= {24'b0, data_rdata_i[15:8]};
+					'b10: data <= {24'b0, data_rdata_i[23:16]};
+					'b11: data <= {24'b0, data_rdata_i[31:24]};     
+				endcase
+			 end 
+		'd5: begin 
+				case (offset)
+					'b00: data <= {16'b0, data_rdata_i[15:0]};
+					'b10: data <= {16'b0, data_rdata_i[31:16]};
+				endcase
+			 end  
+		 default: begin
+					data_be <= 'b0001;
+					data <= 'd0;
+					data_wdata <= 'd0;
+			 end
+	endcase
 	end
 
 
